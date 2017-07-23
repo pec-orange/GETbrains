b8248c55f94e5f04843b6f71a3e6f4fbd24b8f66
pec_orange <pec-orange@mail.ru>
2017-07-23 23:57:19 +0300

    getting started

---
+++ app/src/main/java/shenancalhar/getbrains/MainActivity.java
@@ -0,0 +1,16 @@
+package shenancalhar.getbrains;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+// Generic Exploration Technology is an autonomous mobile robot
+// designed for unknown area exploration and navigation.
+// This module contains "brains" of the G.E.T.
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
