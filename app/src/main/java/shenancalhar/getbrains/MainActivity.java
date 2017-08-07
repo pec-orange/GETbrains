b07e504eac70b94d0180512eb1b8073b7bf7b31c
pec_orange <pec-orange@mail.ru>
2017-08-07 18:17:46 +0300

    interface fix

--- app/src/main/java/shenancalhar/getbrains/MainActivity.java
+++ app/src/main/java/shenancalhar/getbrains/MainActivity.java
@@ -1,7 +1,10 @@
 package shenancalhar.getbrains;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ToggleButton;
 
 // Generic Exploration Technology is an autonomous mobile robot
 // designed for unknown area exploration and navigation.
@@ -12,5 +15,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void toggleButtonLED_Click(View v){
+        System.out.println("Button click!");
+    }
+
+    @Override
+    public void onBackPressed() {
+        System.out.println("exitButton click");
+        //closeAccessory();
+        // The safest way to close the application
+        Intent homeIntent = new Intent(Intent.ACTION_MAIN);
+        homeIntent.addCategory( Intent.CATEGORY_HOME );
+        homeIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(homeIntent);
+        System.exit(0);
     }
 }
